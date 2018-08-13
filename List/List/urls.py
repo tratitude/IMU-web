630e256b1754baae88f9fb59d4a20dcb8ce33529
Ridost <liao60209@gmail.com>
2018-08-13 22:20:53 +0800

    List

--- List/List/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from ItemList.views import listone,list
-
-urlpatterns = [
-    path('listone/', listone),
-    path('list/', list),
-    path('admin/', admin.site.urls),
-]
