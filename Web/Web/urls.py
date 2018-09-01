9c6778a81bb83e4ce4c439ea48e579345fae2aa4
weiber-hsu <39873242+weiber-hsu@users.noreply.github.com>
2018-09-01 23:51:32 +0800

    login,logout and signup

--- Web/Web/urls.py
+++ Web/Web/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from account import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('login/', views.login),
+	path('logout/', views.logout),
+	path('index/', views.index),
+	path('sign/', views.sign),
 ]
--- Web/account/views.py
+++ Web/account/views.py
@@ -1,14 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.contrib.auth.models import User
+from account.models import user_detail
 # Create your views here.
 def index(request):
-
+	if request.user.is_authenticated:
+	   name=request.user.username
+	   message="登入成功"
+	   return render(request, "index.html", locals())
+	else:		
+		return redirect("/login/")			
+		
 def login(request):
-
+	if request.method == 'POST':
+		name = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=name, password=password)
+		if user is not None:
+			if user.is_active:
+				auth.login(request,user)
+				return redirect("/index/")			
+			else:
+				message = '帳號尚未啟用！'
+				return render(request, "login.html", locals())
+		else:
+			message = '登入失敗！'
+	return render(request, "login.html", locals())
+	
 def logout(request):
-
-def singup(request):
-
+	auth.logout(request)	
+	return redirect('/login/')
+	message="登出成功"
+	
+def sign(request):
+	if request.method == 'POST':
+		name = request.POST['username']
+		password = request.POST['password']
+		password2 = request.POST['password2']
+		mail = request.POST['email']
+		user_phone= request.POST['phone']
+		if password!=password2:
+			message="請輸入相同密碼"
+			return render(request,"sign.html",locals())
+		try:
+			user=User.objects.get(username=name)
+		except:
+			user=None
+		try:
+			usermail=User.objects.get(email=mail)
+		except:
+			usermail=None
+		if user!=None :
+			message="帳號已有人使用"
+			return render(request, "sign.html", locals())
+		elif usermail!=None:
+			message="信箱已有人使用"
+			return render(request, "sign.html", locals())
+		else :	
+			user=User.objects.create_user(name, mail, password)
+			user.is_active=True
+			user.save()
+			detail=user_detail.objects.create(user=user,phone=user_phone)
+			return redirect('/login/')
+	return render(request, "sign.html", locals())
