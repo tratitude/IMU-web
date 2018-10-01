05fea1d876763040accd1eda7fee54009fde2d12
weiber-hsu <39873242+weiber-hsu@users.noreply.github.com>
2018-10-02 00:04:52 +0800

    admin modify
    123

--- Web/account/admin.py
+++ Web/account/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
+#from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+#from django.contrib.auth.models import User
 
-from account.models import user_detail
+from .models import user_detail
+admin.site.register(user_detail)
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
-class accountInline(admin.StackedInline):
+"""class accountInline(admin.StackedInline):
     model = user_detail
     can_delete = False
     verbose_name_plural = 'user_detail'
@@ -15,3 +16,5 @@ class accountInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (accountInline, )
 # Register your models here.
+"""
+
